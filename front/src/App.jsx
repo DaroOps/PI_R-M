ee0f85221e4e40d87ac0ca23f541e9383154c91d
DaroOps <romerosd089@hotmail.com>
2023-09-14 18:34:35 -0500

    homework finish, bug close button

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
-import Card from '../../../FT-M2/06-React-Intro/homework/02 - Integration/src/components/Card';
-import Cards from '../../../FT-M2/06-React-Intro/homework/02 - Integration/src/components/Cards';
+import Card from './components/Card';
+import Cards from './components/Cards';
+import SearchBar from './components/SearchBar';
 import characters, { Rick } from './data.js';
 
 
@@ -9,17 +10,8 @@ function App() {
     <div className='App'>
          <SearchBar onSearch={(characterID) => window.alert(characterID)} />
          <Cards characters={characters} />
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
-      </div>
+      
+    </div>
   )
 }
 
