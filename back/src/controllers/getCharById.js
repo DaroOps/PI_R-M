3014c782d30be36e81e45606fbaf187e90a1bc04
DaroOps <romerosd089@hotmail.com>
2023-10-11 14:58:41 -0500

    fixed error in getCharById found by jest, updated config in  package.json

--- back/src/controllers/getCharById.js
+++ back/src/controllers/getCharById.js
@@ -7,19 +7,24 @@ const getCharById = async (req, res) => {
 
     try {
         const data = (await axios(`${URL}/${id}`)).data;
-        const character = {
-            id,
-            name: data.name,
-            origin: data.origin,
-            status: data.status,
-            gender: data.gender,
-            species: data.species,
-            location: data.location,
-            image: data.image
+        if(data.id){
+
+            const character = {
+                id,
+                name: data.name,
+                origin: data.origin,
+                status: data.status,
+                gender: data.gender,
+                species: data.species,
+                location: data.location,
+                image: data.image
+            };
+
+            return res.status(200).json(character);
         }
-        return res.status(200).json(character);
+        return res.status(404).send('Not found');
     } catch (error) {
-        throw Error(error.message);
+        return res.status(500).send(error.message);
     }
 
 }
