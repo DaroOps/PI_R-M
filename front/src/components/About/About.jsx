08b0c3d0004ecdee8db2612ac44e8f1f22a8b122
DaroOps <romerosd089@hotmail.com>
2023-10-17 12:15:05 -0500

    finished about section go with the detail section

--- front/src/components/About/About.jsx
+++ front/src/components/About/About.jsx
@@ -5,10 +5,39 @@ const About = () => {
     return (
 
         <div className="about">
+            <img className="about-background" src="https://r4.wallpaperflare.com/wallpaper/979/756/6/rick-and-morty-tv-adult-swim-rick-sanchez-wallpaper-ff3c12efadb9450847ad72640483b050.jpg" />
             <div className="about-spacer" />
-            <h2>ABOUT ME</h2>
-            <div className='hide-heatmap-content'>
-                <Heatmap className='heatmap' />
+            <div className='about-adjust-size'>
+                <div className='about-bgcard'>
+                    <div className='header-text'> <h2>David Romero - (DaroOPs)</h2></div>
+                    <div className='head-card'>
+                        <div className='dev-icon'></div>
+                        <div className='hide-heatmap-content'>
+                            <Heatmap className='heatmap' />
+                        </div>
+                    </div>
+                    <div className='about-info'>
+                        <h2 className='spot-title'>About</h2>
+                       
+                        <p className='about-info-spot'>
+                            My name is David, and I am a dedicated individual with a profound passion for creativity and innovation. I hold a deep reverence for technology and have immersed myself in various technologies, including Python, C#, and Java. My primary objective is to amalgamate the realms of technology and creativity to enhance the projects in which I am involved. My aspiration is to establish a compelling synergy that adeptly resolves challenges and serves as a source of inspiration.
+                        </p>
+                        <br />
+                    </div>
+                    
+                    <div className="about-media">
+                        <div className="li">
+                            <a href="#" className="github-icon">
+                                <i className="fa-brands fa-github"></i>
+                            </a>
+                        </div>
+                        <div className="li">
+                            <a href="#" className="linkedin-icon">
+                                <i className="fa-brands fa-linkedin"></i>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
