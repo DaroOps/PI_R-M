19ec7681e5f3760c2637f1cd75b511ec14fb793d
DaroOps <romerosd089@hotmail.com>
2023-10-18 22:38:42 -0500

    created db Models using ORM(sequelize)

---
+++ back/src/models/User.js
@@ -0,0 +1,11 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+   sequelize.define('User', {
+         id:{
+            type:DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true
+         }
+   }, { timestamps: false });
+};
